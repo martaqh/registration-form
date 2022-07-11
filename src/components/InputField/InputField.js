92f19d75428f6f49efc4c2807c2d858307e2c947
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-11 22:42:37 +0200

    Add InputField component

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -2,7 +2,7 @@ import styles from './InputField.module.scss'
 
 const InputField = props => {
     return (
-        <div>
+        <div className={styles.inputField}>
             <label>{props.label}</label>
             <input type={props.type} placeholder={props.placeholder}/>
         </div>
