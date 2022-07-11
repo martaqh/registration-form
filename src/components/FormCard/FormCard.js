be797ab73f41302afcb6f8be921b2f041069ec5c
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-11 23:38:18 +0200

    Add styles to FormCard

--- src/components/FormCard/FormCard.js
+++ src/components/FormCard/FormCard.js
@@ -1,6 +1,7 @@
 import styles from './FormCard.module.scss';
 import eye from '../../images/eye.svg';
 import InputField from '../InputField/InputField';
+import ValidationMessage from '../ValidationMessage/ValidationMessage';
 
 
 const FormCard = () => {
@@ -8,7 +9,9 @@ const FormCard = () => {
         <div className={styles.formCard}>
             <InputField label="E-mail" type="email" placeholder="Somthing ending with monterail.com" />
             <InputField label="Password" type="password" placeholder="Enter your password" />
-            
+            <ValidationMessage>At least 8 characters</ValidationMessage>
+            <ValidationMessage>At least one letter</ValidationMessage>
+            <ValidationMessage>At least one digit</ValidationMessage>
         </div>
     )
 }
