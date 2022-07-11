1a906b62cd8fd5bd6c6854d4720896fafc637094
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-11 19:29:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import Header from "./components/Header/Header";
+import PageTitle from './components/PageTitle/PageTitle'
+import Container from './components/Container/Container'
+import PageSubtitle from "./components/PageSubtitle/PageSubtitle";
+import FormCard from "./components/FormCard/FormCard";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Header />
+      <Container>
+        <PageTitle>Ahoy you!</PageTitle>
+        <PageSubtitle>Care to register?</PageSubtitle>
+        <FormCard />
+      </Container>
+    </div>
   );
 }
 
