b3efe52020ed35fcf2bdf4cdc540b4a209416fde
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-12 23:47:57 +0200

    Add Message component

---
+++ src/components/Message/Message.js
@@ -0,0 +1,12 @@
+import styles from './Message.module.scss';
+
+const Message = props => {
+    return (
+        <div className={styles.message__success}>
+                <p >We have sent you an email to <span>{props.email}</span>.</p>
+                <p>Make sure to click the link from the message to activate your account. </p>
+            </div>
+    )
+}
+
+export default Message;
