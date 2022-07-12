48a054f6e16270b1e5b322abbb6e82022941c5fd
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-12 23:46:56 +0200

    Update FormCard component

--- src/components/FormCard/FormCard.js
+++ src/components/FormCard/FormCard.js
@@ -1,18 +1,12 @@
 import styles from './FormCard.module.scss';
-import eye from '../../images/eye.svg';
-import InputField from '../InputField/InputField';
-import ValidationMessage from '../ValidationMessage/ValidationMessage';
 
 
-const FormCard = () => {
+
+const FormCard = props => {
     return (
-        <div className={styles.formCard}>
-            <InputField label="E-mail" type="email" placeholder="Somthing ending with monterail.com" />
-            <InputField label="Password" type="password" placeholder="Enter your password" />
-            <ValidationMessage>At least 8 characters</ValidationMessage>
-            <ValidationMessage>At least one letter</ValidationMessage>
-            <ValidationMessage>At least one digit</ValidationMessage>
-        </div>
+        <form className={styles.formCard} onSumbit={props.onSumbit}>
+            {props.children}
+        </form>
     )
 }
 
