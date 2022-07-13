864cf0c263eaea3fd438b4e419d0c360e700b66a
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-13 18:25:12 +0200

    Add birth date handling

--- src/components/pages/PersonalData/PersonalData.js
+++ src/components/pages/PersonalData/PersonalData.js
@@ -9,19 +9,32 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addUserData } from '../../../redux/userDataRedux';
 import { useNavigate } from 'react-router';
+import PrivacyPolicyCheck from "../../PrivacyPolicyCheck/PrivacyPolicyCheck";
 
 const RegistrationPersonalData = () => {
 
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    
+    const [birthDate, setBirthDate] = useState()
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    console.log(birthDate)
 
     const userPersonalData = {
         firstName: firstName,
         lastName: lastName,
+        birthDate: birthDate,
     }
+
+    const dateOfBirth = new Date(birthDate);
+    const monthDifference = Date.now() - dateOfBirth.getTime();
+    const ageInDateFormat = new Date(monthDifference); 
+    const year = ageInDateFormat.getUTCFullYear();
+    const age = Math.abs(year - 1970);
+
+    console.log('age:', age)
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -51,9 +64,19 @@ const RegistrationPersonalData = () => {
                         value={lastName}
                         onChange={e => setLastName(e.target.value)}
                     />
-                    <InputField label="Date of birth" type="date" placeholder="DD/MM/YYYY"></InputField>
-                    <ValidationMessage>You should be minimum 18 years old</ValidationMessage>
-                
+                    <InputField
+                        label="Date of birth"
+                        type="date"
+                        placeholder="DD/MM/YYYY"
+                        value={birthDate}
+                        onChange={e => setBirthDate(e.target.value)}
+                    ></InputField>
+                    <ValidationMessage
+                        className={age >= 18 ? "valid" : null}
+                    >
+                        You should be minimum 18 years old
+                    </ValidationMessage>
+                    <PrivacyPolicyCheck></PrivacyPolicyCheck>
                     <ButtonsSection href="/success" onClick={handleSubmit}></ButtonsSection>
                 </form>
             </FormCard>
