5e64907010532fb719f7568752f3d855fac59f1f
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-13 22:41:36 +0200

    Add styles to PrivacyPolicyCheck component

--- src/components/PrivacyPolicyCheck/PrivacyPolicyCheck.js
+++ src/components/PrivacyPolicyCheck/PrivacyPolicyCheck.js
@@ -1,8 +1,8 @@
 import styles from './PrivacyPolicyCheck.module.scss';
 
-const PrivacyPolicyCheck = props => {
+const PrivacyPolicyCheck = () => {
     return (
-        <div className={styles.checkbox}>
+        <div className={styles.privacyPolicyCheck }>
             <input type="checkbox" name="privacy-policy-acceptance" value="unchecked" />
             <label>I accept <a href="#">Privacy Policy</a></label>
         </div>
