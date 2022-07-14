b08d032ced453ae357f7c73420d53ca125ce2e24
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-14 12:59:20 +0200

    Refactor acceptance checkbox component

---
+++ src/components/common/CheckboxAccept/CheckboxAccept.js
@@ -0,0 +1,13 @@
+import styles from './CheckboxAccept.module.scss';
+
+const CheckboxAccept = props => {
+    return (
+        <div className={styles.checkboxAccept}>
+            <input type="checkbox" name="privacy-policy-acceptance" onChange={props.onChange} />
+            <label>I accept <a href="#">Privacy Policy</a></label>
+        </div>
+        
+    )
+}
+
+export default CheckboxAccept;
